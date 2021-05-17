9b21612d47f4be214250bf564e84f7337340aa40
“BenjaSar” <robotechunter@gmail.com>
2021-05-16 21:30:49 -0300

    Se agregaron dos nuevos tests

--- Lista/src/lista.js
+++ Lista/src/lista.js
@@ -14,17 +14,28 @@ module.exports = class Lista {
         if(this.#elementos.length == 1){
             return this.#elementos[0].valor;
         }
-        if(this.#elementos.clave !== clave || this.#elementos.clave == ""){
-            return this.#elementos.clave = "updatedKey";
+        return NaN
+    }
+
+    add(clave, valor){
+        let updatedKey;
+        this.#elementos.push({clave,valor});
+        this.#elementos.count += 1;
+
+        if(clave !== this.#elementos.clave){
+            return this.#elementos.clave = updatedKey;
         }
         if(typeof(this.#elementos.clave == "string")){
             return this.#elementos.clave;
         }
-        return NaN;
     }
 
-    add(clave, valor){
-        this.#elementos.push({clave,valor});
+    remove(clave, valor){
+        
+        const index = this.#elementos.indexOf(1)
+        if(index>-1){
+            return this.#elementos.slice({clave, valor})
+        }
     }
 
 }
--- Lista/test/test_lista.js
+++ Lista/test/test_lista.js
@@ -1,6 +1,7 @@
 /**** LISTA DE PRUEBAS ***
 - Cuando se agrega un elemento a una lista vacía la lista de claves está ordenada
 - Cuando se agrega un elemento al principio la lista de claves está ordenada
+- Array de claves ordenadas
  */
 
 const assert = require("chai").assert;
@@ -20,7 +21,7 @@ describe("en una lista vacia" , function() {
 describe("Cuando se agrega un elemento a la lista vacia", function(){
     var lista = new Lista();
     lista.add("clave", "valor"); 
-
+    
      it("Hay un elemento", function(){
         assert.equal(lista.count(), 1)
     }) 
@@ -28,12 +29,19 @@ describe("Cuando se agrega un elemento a la lista vacia", function(){
     it("Recuperar un valor a partir de la clave", function(){
         assert.equal(lista.find("clave"), "valor")
     })
-    
     it("Se admite solo texto para clave", function(){
         assert.equal(lista.find(50), "valor")
     })
-
     it("Agregar clave y actualizar clave", function(){
         assert.equal(lista.find("updatedKey"), "valor")
     })
+})
+
+describe("Cuando se elimina un elemento de la lista", function(){
+    var lista = new Lista();
+
+    it("Debería retornar -1 cual el elemento no está en la lista", function(){
+        assert.equal(lista.remove("clave"))
+    })
+
 })
