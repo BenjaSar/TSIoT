0aae0161ddf86f2fa60ef4e763f416ac6a61b2bc
“BenjaSar” <robotechunter@gmail.com>
2021-07-15 18:17:21 -0300

    Se actualizaron pruebas

--- Lista/src/lista.js
+++ Lista/src/lista.js
@@ -1,65 +1,65 @@
 module.exports = class Lista {
-    #elementos; 
-    
-    constructor(clave, valor) {     
-        this.#elementos =[]
-        this.clave = clave;
-        this.valor = valor;
-    }
+  #elementos;
 
-    count(){
-        return this.#elementos.length;
-    }
-    
-    //getter
-    find(clave){
-            var i;
-        if(this.#elementos.length > 0){
-            for (i>0; i<=this.#elementos.length; i++){
-                if(this.#elementos[i].indexOf(clave)>-1){
-                    return this.#elementos[i].valor
-                }      
-            }           
-        } 
-        return NaN;
-    }
+  constructor(clave, valor) {
+    this.#elementos = [];
+    this.clave = clave;
+    this.valor = valor;
+  }
 
-    sort(){
-        this.#elementos.sort(function(c1, c2){
-            if(c1.clave > c2.clave) {
-                return  c1.valor;
-            } else{
-                return c2.valor
-            }
-        })
-    }
+  count() {
+    return this.#elementos.length;
+  }
 
-    add(clave, valor){
-        if(this.#elementos.includes(clave) === false){
-            return this.#elementos.push({clave, valor})
+  //getter
+  find(clave) {
+    var i;
+    if (this.#elementos.length > 0) {
+      for (i > 0; i <= this.#elementos.length; i++) {
+        if (this.#elementos[i].indexOf(clave) > -1) {
+          return this.#elementos[i].valor;
         }
+      }
     }
+    return NaN;
+  }
 
-    isString(){
-        if(this.#elementos.includes("^[a-zA-Z\\s]+$")){
-            return true
-        }
-        return false
-    }
+  sort() {
+    this.#elementos.sort(function (c1, c2) {
+      if (c1.clave > c2.clave) {
+        return c1.valor;
+      } else {
+        return c2.valor;
+      }
+    });
+  }
 
-    //setter
-    actualizar(clave){
-            this.#elementos.forEach((element, index) => {
-                if(element.clave != clave){
-                    return this.#elementos[index] = clave
-                }
-            })
+  add(clave, valor) {
+    if (this.#elementos.includes(clave) === false) {
+      return this.#elementos.push({ clave, valor });
     }
-    
-    remove(clave){
-        const index = this.#elementos.indexOf(clave)
-        while(index>-1){
-            return this.#elementos.splice(index,1)          
-        }
+  }
+
+  isString() {
+    if (this.#elementos.includes("^[a-zA-Z\\s]+$")) {
+      return true;
     }
-}
+    return false;
+  }
+
+  //setter
+  actualizar(clave) {
+    this.#elementos.forEach((element, index) => {
+      if (element.clave != clave) {
+        return (this.#elementos[index] = clave);
+      }
+    });
+  }
+
+  remove(clave) {
+    const index = this.#elementos.indexOf(clave);
+    while (index > -1) {
+      return this.#elementos.splice(index, 1);
+    }
+  }
+};
